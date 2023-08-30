3eca288f1a1ea4709c9258f58ba57d16413bfcc0
GG <qq1270414897@gmail.com>
2023-08-30 21:26:58 +0800

    修改命名规则+完成代码质量部分内容学习

---
+++ 02-代码质量/01-JSDoc.js
@@ -0,0 +1,12 @@
+// 标准的函数注释语法：JSDoc
+/**
+ * 返回x的n次幂
+ * @date 8/30/2023 - 8:45:58 PM
+ *
+ * @param {*} x 参数
+ * @param {*} n 幂次
+ * @returns {number} x的n次方
+ */
+function pow(x, n) {
+    return x ** n;
+}
